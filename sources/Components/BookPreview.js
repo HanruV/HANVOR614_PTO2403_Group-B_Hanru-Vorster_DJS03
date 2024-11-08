4b1f7838f3adf7b2ca38b3d1e4a12787b1bee9a9
Hanru Vorster <hanru.vorster@gmail.com>
2024-11-08 13:52:46 +0200

    added html structure for the render of BookPreview

--- sources/Components/BookPreview.js
+++ sources/Components/BookPreview.js
@@ -18,7 +18,14 @@ class BookPreview extends HTMLElement {
     const author = this.getAttribute("author");
     const id = this.getAttribute("id");
 
-    this.shadowRoot.innerHTML = ``;
+    this.shadowRoot.innerHTML = /*html*/ `
+    <button class="preview" data-preview="${id}">
+        <img class="preview__image" src="${image}" />
+        <div class="preview__info">
+          <h3 class="preview__title">${title}</h3>
+          <div class="preview__author">${author}</div>
+        </div>
+      </button>`;
   }
 }
 
